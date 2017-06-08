8f3a1a38fd6bfb2e64e8cbd9c67a6c669b3c0495
nemba246 <nemba246@gmail.com>
2017-06-08 10:02:18 -0700

    image now tilts in response to motion

--- platforms/android/assets/www/js/index.js
+++ platforms/android/assets/www/js/index.js
@@ -45,8 +45,66 @@ function onDeviceReady () {
 	
 	//gyro();
 	
-	context = myCanvas.getContext('2d');
-	setInterval(draw,10);
+	
+	//context = myCanvas.getContext('2d');
+	//setInterval(draw,10);
+	
+	console.log("first one");
+	
+
+
+	
+	if (window.DeviceOrientationEvent) {
+		
+		console.log("inside the stuff");
+		
+		document.getElementById("doEvent").innerHTML = "DeviceOrientation";
+		// Listen for the deviceorientation event and handle the raw data
+		
+		
+		window.addEventListener('deviceorientation', function(eventData) {
+			// gamma is the left-to-right tilt in degrees, where right is positive
+			var tiltLR = eventData.gamma;
+
+			// beta is the front-to-back tilt in degrees, where front is positive
+			var tiltFB = eventData.beta;
+
+			// alpha is the compass direction the device is facing in degrees
+			var dir = eventData.alpha;
+
+			//console.log(dir);
+			
+			// call our orientation event handler
+			//deviceOrientationHandler(tiltLR, tiltFB, dir);
+			
+			
+			
+			document.getElementById("doTiltLR").innerHTML = Math.round(tiltLR);
+
+
+			console.log(tiltLR);
+			document.getElementById("doTiltFB").innerHTML = Math.round(tiltFB);
+			document.getElementById("doDirection").innerHTML = Math.round(dir);
+
+			// Apply the transform to the image
+			var logo = document.getElementById("imgLogo");
+			logo.style.webkitTransform =
+			"rotate("+ tiltLR +"deg) rotate3d(1,0,0, "+ (tiltFB*-1)+"deg)";
+			logo.style.MozTransform = "rotate("+ tiltLR +"deg)";
+			logo.style.transform =
+			"rotate("+ tiltLR +"deg) rotate3d(1,0,0, "+ (tiltFB*-1)+"deg)";
+				
+				
+					
+		}, false);
+	  
+	
+
+	} else {
+		document.getElementById("doEvent").innerHTML = "Not supported."
+	}
+
+
 	
 }
 
